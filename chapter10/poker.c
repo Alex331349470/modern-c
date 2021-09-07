2b85abff340ff865eea8254d861e3076671f7539
Alex <zhengyouchen@gmail.com>
2021-09-07 12:53:26 +0800

    test for vim

--- chapter10/poker.c
+++ chapter10/poker.c
@@ -6,7 +6,7 @@
 #define NUM_RANKS 13
 #define NUM_SUITS 4
 #define NUM_CARDS 5
-//external variable
+
 int num_in_rank[NUM_RANKS];
 int num_in_suit[NUM_SUITS];
 bool straight, flush, four, three;
--- chapter4/upc.c
+++ chapter4/upc.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <aio.h>
 
 int main(void)
 {
