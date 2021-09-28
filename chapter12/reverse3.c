a6b936d2a110b1ead49a6ca3b76d4095a1e98823
Alex <zhengyouchen@gmail.com>
2021-09-28 09:57:22 +0800

    add chapter12

--- chapter12/reverse3.c
+++ chapter12/reverse3.c
@@ -2,6 +2,17 @@
 
 #define N 10
 
+int read_line(char str[], int n)
+{
+    int ch, i = 0;
+
+    while ((ch = getchar()) != '\n')
+        if (i < n)
+            str[i++] = ch;
+    str[i] = '\0';
+    return i;
+}
+
 int main(void)
 {
 	int a[N], *p;
