ffa34a7b7d41c71bb4970776074c8ec432d34827
Alex <zhengyouchen@gmail.com>
2021-10-10 22:39:44 +0800

    change test file form chapter13

--- chapter13/test.c
+++ chapter13/test.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-#define N 10
-
 int main(void)
 {
-    int a[N], sum, *p;
-    sum = 0;
+    printf("Hello world!\n");
 
-    for (p = &a[0]; p < &a[N]; p++)
-        sum += *p;
-    
     return 0;
 }
