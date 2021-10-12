ce1b20dd166712c1169735a01b127998a17e40f8
Alex <zhengyouchen@gmail.com>
2021-10-12 19:11:55 +0800

    test for array pointer

---
+++ chapter12/test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define N 10
+
+int find_largest(int a[], int n);
+
+int main(void)
+{
+	int a[N], *p;
+
+	printf("Enter %d numbers: ", N);
+	for (p = a; p < a + N; ++p)
+		scanf("%d", p);
+
+	printf("The largest number is %d\n", find_largest(a, N));
+
+	return 0;
+}
+
+int find_largest(int a[], int n)
+{
+	int max = a[0], *p;
+
+	for (p = a; p < a + n; p++)
+		if (*p > max)
+			max = *p;
+	
+	return max;
+}
