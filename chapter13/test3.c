8e5d1be5d5caf52b829a241f27a7bc96fed1a507
Alex <zhengyouchen@gmail.com>
2021-10-14 22:24:39 +0800

    change some thing for test3.c of chapter13

--- chapter13/test3.c
+++ chapter13/test3.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #define N 10
+
+int get_largest(int a[], int n);
 int main(void)
 {
 	int a[N], *p;
 
-	printf("%s\n", );
+	printf("Enter %d numbers: ", N);
+
+	for (p = a; p < a + N; p++)
+		scanf("%d", p);
+
+	printf("The largest number is %d\n", get_largest(a, N));
+
+	return 0;
+}
+
+int get_largest(int a[], int n)
+{
+	int max = a[0], *p;
+
+	for (p = a; p < a + n; p++)
+		if (max < *p)
+			max = *p;
+
+	return max;
 }
