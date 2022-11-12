050b6f0be1cc8f161204c084b9e2502470ed19b3
Alex Chan <331349470@qq.com>
2022-11-12 15:26:29 +0800

    modify the test file

--- chapter12/test.c
+++ chapter12/test.c
@@ -3,10 +3,10 @@
 int main (void)
 {
     int n;
-    printf("Enter a integer:");
+    printf("Enter a integer: ");
     scanf("%d", &n);
 
-    printf("The input integer is %d.\n", n);
+    printf("The input integer is %d\n", n);
 
     return 0;
 }
