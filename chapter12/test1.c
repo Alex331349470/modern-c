c1fd6cee4fedecfd2ecb82303ee3589fbfd499ba
Alex Chan <331349470@qq.com>
2022-12-16 22:48:01 +0800

    review the chapter11

---
+++ chapter12/test1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int num;
+    printf("Enter a integer:");
+    scanf("%d", &num);
+
+    printf("The input number is %d\n", num);
+
+    return 0;
+}
+
