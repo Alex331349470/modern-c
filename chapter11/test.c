33064810a6675d502fb1a1e80619dd90119e115a
Alex Chan <331349470@qq.com>
2022-12-19 19:42:09 +0800

    review the chapter11

---
+++ chapter11/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Hello chapter11\n");
+
+    return 0;
+}
+
---
+++ chapter11/test.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# testing zsh script for double and float calculate
+
+value1=2.5
+value2=3.4
+
+echo $[ $value1 * $value2 ]
+exit
