c7888bf8bdd168fa158a94762cfb0654b43f21ae
Alex Chan <331349470@qq.com>
2023-01-02 10:19:08 +0800

    test some thing

--- chapter11/test.c
+++ chapter11/test.c
@@ -2,7 +2,7 @@
 
 int main (void)
 {
-    printf("Hello chapter11\n");
+    printf("Hello world!\n");
 
     return 0;
 }
---
+++ chapter11/test2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int n;
+    printf("Enter a integer: ");
+    scanf("%d", &n);
+
+    printf("The input number is %d\n", n);
+
+    return 0;
+}
