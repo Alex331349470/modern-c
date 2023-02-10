f7811fbfb1819968d93a00fd815ffa131679cc1b
Alex Chan <331349470@qq.com>
2023-02-10 22:11:22 +0800

    repush the pun program

--- chapter2/pun.c
+++ chapter2/pun.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-//main function
-int main (void)
-{
-    //print a pun
-    printf("To C, or not to C: that is the question.\n");
+int main(void) {
 
+    printf("To C, or not to C: that is the question.\n");
+    
     return 0;
 }
