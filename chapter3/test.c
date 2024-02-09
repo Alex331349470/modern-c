b0871a2b9fd63fcc8d6163e7858d262d82ede9db
Alex Chan <331349470@qq.com>
2024-02-09 22:58:31 +0800

    something terrible

--- chapter3/test.c
+++ chapter3/test.c
@@ -2,11 +2,7 @@
 
 int main (void)
 {
-    int i = 30;
-    float x = 8839.30f;
-
-    printf("%d\n", i);
-    printf("%f\n", x);
+    printf("Programming is fun.\n");
 
     return 0;
 }
---
+++ chapter3/test2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Enter a integer: ");
+    int n;
+    scanf("%d", &n);
+
+    printf("The input number is %d\n", n);
+
+    return 0;
+}
