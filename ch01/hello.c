af67ba6495bef0c34b59c98a6333ea5545e901ba
= <=>
2024-12-10 13:38:16 +0800

    test something new

--- ch01/hello.c
+++ ch01/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(void) {
-  printf("Hello, world!\n");
+int main(int argc, char const *argv[])
+{
+  printf("Hello, World!\n");
+  printf("The square root of 2.0 is %f\n", sqrt(2.0));
+  printf("The natural logarithm of 2.0 is %f\n", log(2.0));
 
   return 0;
 }
