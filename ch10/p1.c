c657acfdfb8f0338edea03d16543039060610862
alex chan <zhengyouchen@gmail.com>
2025-01-17 16:29:29 +0800

    save the ch10 project 01 's change

--- ch10/p1.c
+++ ch10/p1.c
@@ -4,37 +4,25 @@
 
 #define STACK_SIZE 100
 
-int contents[STACK_SIZE];
+char contents[STACK_SIZE];
 int top = 0;
 
-void make_empty(void) { top = 0; }
+void make_empty(void);
 
-bool is_full(void) { return top == STACK_SIZE; }
+bool is_full(void);
 
-bool is_empty(void) { return top == 0; }
+bool is_empty(void);
 
-void stack_overflow(void) {
-  printf("Expression is too complex\n");
-  exit(EXIT_FAILURE);
+void push(char);
+
+char pop(void);
+
+void stack_overflow(void);
+
+void stack_underflow(void);
+
+int main(void) {
+  char c;
+
+  printf("Enter parenteses and/or braces: ");
 }
-
-void stack_underflow(void) {
-  printf("Not enough operands in expression\n");
-  exit(EXIT_FAILURE);
-}
-
-void push(int i) {
-  if (is_full())
-    stack_overflow();
-  else
-    contents[top++] = i;
-}
-
-int pop(void) {
-  if (is_empty())
-    stack_underflow();
-  else
-    return contents[--top];
-}
-
-int main(void) {}
