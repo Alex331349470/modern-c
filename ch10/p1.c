9327b3d2d2337188561debdcaac7f4e27654e847
alex chan <zhengyouchen@gmail.com>
2025-01-22 09:38:37 +0800

    almost done the ch10, I am really boring of this chapter

--- ch10/p1.c
+++ ch10/p1.c
@@ -1,9 +1,54 @@
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define STACK_SIZE 100
 
-int contents[STACK_SIZE];
+char contents[STACK_SIZE];
 int top = 0;
+
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(char i);
+char pop(void);
+void stack_overflow(void);
+void stack_underflow(void);
+
+int main(void)
+{
+    char ch;
+    printf("Enter parenteses and/or braces: ");
+
+    while ((ch = getchar()) != '\n')
+    {
+        if (ch == '(' || ch == '{')
+            push(ch);
+        else if (ch == ')' || ch == '}')
+        {
+            if (is_empty())
+                stack_underflow();
+            if (is_full())
+                stack_overflow();
+            else
+            {
+                char temp = pop();
+                if ((ch == ')' && temp != '(') || (ch == '}' && temp != '{'))
+                {
+                    printf("Parenteses/braces are not nested properly\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+
+    if (is_empty())
+        printf("Parenteses/braces are nested properly\n");
+    else
+        printf("Parenteses/braces are not nested properly\n");
+
+    exit(EXIT_SUCCESS);
+}
 
 void make_empty(void)
 {
@@ -20,7 +65,7 @@ bool is_full(void)
     return top == STACK_SIZE;
 }
 
-void push(int i)
+void push(char i)
 {
     if (is_full())
         stack_overflow();
@@ -28,10 +73,22 @@ void push(int i)
         contents[top++] = i;
 }
 
-int pop(void)
+char pop(void)
 {
     if (is_empty())
         stack_underflow();
-    else
-        return contents[--top];
+
+    return contents[--top];
+}
+
+void stack_overflow(void)
+{
+    printf("Stack overflow\n");
+    exit(EXIT_FAILURE);
+}
+
+void stack_underflow(void)
+{
+    printf("Stack underflow\n");
+    exit(EXIT_FAILURE);
 }
