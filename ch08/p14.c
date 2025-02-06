816012f1d2dbb789ad3315e842c5bc805ceb6170
alex chan <zhengyouchen@gmail.com>
2025-02-06 11:36:38 +0800

    change some format of file

--- ch08/p14.c
+++ ch08/p14.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 
-int main(void)
-{
-    char s[100];
-    char c;
-    int i = 0, j = 0;
+int main(void) {
+  char s[100];
+  char c;
+  int i = 0, j = 0;
 
-    printf("Enter a sentence: ");
+  printf("Enter a sentence: ");
 
-    for (; i < 100; i++) {
-        s[i] = getchar();
-        if (s[i] == '.' || s[i] == '!' || s[i] == '?') {
-            c = s[i];
-            printf("%d\n", i);
-            s[i] = '\0';
-            break;
-        }
+  for (; i < 100; i++) {
+    s[i] = getchar();
+    if (s[i] == '.' || s[i] == '!' || s[i] == '?') {
+      c = s[i];
+      printf("%d\n", i);
+      s[i] = '\0';
+      break;
+    }
+  }
+
+  printf("Reversal of sentence: ");
+
+  while (i >= 0) {
+    while (s[i] != ' ' && i >= 0)
+      i--;
+
+    j = i + 1;
+
+    while (s[j] != ' ' && s[j] != '\0') {
+      printf("%c", s[j]);
+      j++;
     }
 
-    printf("Reversal of sentence: ");
-
-    while (i >= 0) {
-        while (s[i] != ' ' && i >= 0)
-            i--;
-
-        j = i + 1;
-
-        while (s[j] != ' ' && s[j] != '\0') {
-            printf("%c", s[j]);
-            j++;
-        }
-
-        if (i >= 0) {
-            printf(" ");
-            i--;
-        }
+    if (i >= 0) {
+      printf(" ");
+      i--;
     }
+  }
 
-    printf("%c\n", c);
+  printf("%c\n", c);
 
-    return 0;
+  return 0;
 }
