d16d0e53375f12964ce74821bf3dafd6ac8fbf87
Alex Chan <zhengyouchen@gmail.com>
2025-05-09 11:00:26 +0800

    rewarm hands for programming language c

--- ch01/hello_world.c
+++ ch01/hello_world.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int foo(void) {
-  printf("Hello, World!\n");
-  return EXIT_SUCCESS;
-}
 
 int main(void) {
-  foo();
-  return EXIT_SUCCESS;
+  printf("Hello, World!\n");
+
+  return 0;
 }
